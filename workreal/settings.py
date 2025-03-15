5a54ebbe3896a031d001ed044de6df2eea1642d2
SimonHarrisWorks <simonharriswork@outlook.com>
2025-03-15 04:10:58 +0000

    directory css issues being fixed take 2

--- workreal/settings.py
+++ workreal/settings.py
@@ -121,16 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
+STATIC_URL = 'logins/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'logins', 'static')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'logins', 'media')
 
